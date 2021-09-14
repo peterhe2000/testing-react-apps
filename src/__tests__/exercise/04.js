b1f7f7777b2aba129aa912919cfeadb5dacab4cc
peterhe <peter.he@sandstone.com.au>
2021-09-15 09:58:32 +1000

    04 extra-2 done. Use faker to mock username and password

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -5,12 +5,17 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
+import * as faker from 'faker'
+
+const buildLoginForm = () => ({
+  username: faker.internet.userName(),
+  password: faker.internet.password(),
+})
 
 test('submitting the form calls onSubmit with username and password', () => {
   const handleSubmit = jest.fn()
   render(<Login onSubmit={handleSubmit} />)
-  const username = 'chucknorris'
-  const password = 'i need no password'
+  const {username, password} = buildLoginForm()
 
   userEvent.type(screen.getByLabelText(/username/i), username)
   userEvent.type(screen.getByLabelText(/password/i), password)
