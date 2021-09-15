bea732d364f39100fd4a36afb1b97eb38755f805
peterhe <peter.he@sandstone.com.au>
2021-09-15 10:11:23 +1000

    04 extra-4 done. Use test-data-bot to fake test data

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -5,18 +5,19 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
-import faker from 'faker'
+import {build, fake} from '@jackfranklin/test-data-bot'
 
-const buildLoginForm = overrides => ({
-  username: faker.internet.userName(),
-  password: faker.internet.password(),
-  ...overrides,
+const buildLoginForm = build({
+  fields: {
+    username: fake(f => f.internet.userName()),
+    password: fake(f => f.internet.password()),
+  },
 })
 
 test('submitting the form calls onSubmit with username and password', () => {
   const handleSubmit = jest.fn()
   render(<Login onSubmit={handleSubmit} />)
-  const {username, password} = buildLoginForm({password: 'abc'})
+  const {username, password} = buildLoginForm()
 
   userEvent.type(screen.getByLabelText(/username/i), username)
   userEvent.type(screen.getByLabelText(/password/i), password)
