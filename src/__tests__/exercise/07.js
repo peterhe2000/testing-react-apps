b4326cbb78dd2759733ad3828721986fe83da257
peterhe <peter.he@sandstone.com.au>
2021-09-18 21:44:44 +1000

    07 extra-1 done. Use testing libraray wrapper to pass a React Component as the wrapper option to have it rendered around the inner element.

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -7,16 +7,26 @@ import {ThemeProvider} from '../../components/theme'
 import EasyButton from '../../components/easy-button'
 
 test('renders with the light styles for the light theme', () => {
-  render(
-    <ThemeProvider>
-      <EasyButton>Easy</EasyButton>
-    </ThemeProvider>,
-  )
-
+  const Wrapper = ({children}) => {
+    return <ThemeProvider initialTheme="light">{children}</ThemeProvider>
+  }
+  render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
   const button = screen.getByRole('button', {name: /easy/i})
   expect(button).toHaveStyle(`
     background-color: white;
     color: black;
+  `)
+})
+
+test('renders with the dark styles for the dark theme', () => {
+  const Wrapper = ({children}) => {
+    return <ThemeProvider initialTheme="dark">{children}</ThemeProvider>
+  }
+  render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: black;
+    color: white;
   `)
 })
 
